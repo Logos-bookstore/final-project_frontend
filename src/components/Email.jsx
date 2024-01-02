8c1f12a8259dac8efbcda47d12034385036f145d
Sam <sam234@yahoo.com>
2024-01-02 15:37:05 +0100

    Sammy: adding Components/Context files

---
+++ src/components/Email.jsx
@@ -0,0 +1,10 @@
+export default function Email() {
+  return (
+    <>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+      </div>
+    </>
+  );
+}
---
+++ src/components/Upload.jsx
@@ -0,0 +1,9 @@
+export default function Upload() {
+  return (
+    <>
+      <div>
+        <button type="submit">Upload</button>
+      </div>
+    </>
+  );
+}
