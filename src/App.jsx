a84ba0cf2ccf58c4167eb9a78991dcb643dac1ec
anonymous <email>
2024-01-02 14:13:57 +0100

    create: imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
 import './App.css';
+import { useContext } from 'react';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Books from './pages/Books';
+import Genre from './pages/Genre';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
 
 function App() {
   return <></>;
