2cbec712d82bba6fe2a878b108c49c381e92e8dc
anonymous <email>
2024-01-03 09:10:39 +0100

    create: files

---
+++ src/pages/Books.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { NavLink, Outlet } from "react-router-dom";
+
+export default function Books() {
+    const [genres, setGenres] = useState([]);
+    useEffect(() => {
+        async function getGenres() {
+            try {
+                const response = await fetch(`${import.meta.env.VITE_GENRES}`);
+                if(response.ok) {
+                    const data = await response.json();
+                    if(data.success) {
+                        console.log(data)
+                        setGenres(data.data);
+                    };
+                };
+            } catch (error) {
+                //
+            }
+        };
+        getGenres();
+    }, []);
+    return (
+        <>
+            <h2>Books</h2>
+            <div>
+                {
+                    genres.map(genre => {
+                        return (
+                            <>
+                                <h3 key={genre._id}><NavLink to={`/books/${genre.genre}`} state={genre.genre}>{genre.genre}</NavLink></h3>
+                            </>
+                        );
+                    })
+                }
+            </div>
+            <Outlet/>
+        </>
+    );
+};
---
+++ src/pages/Login.jsx
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import Continue from "../components/Continue";
+import Email from "../components/Email";
+import Password from "../components/Password";
+import { Context } from "../context/Context";
+
+export default function Login() {
+    const {setUser} = useContext(Context);
+    const navigate = useNavigate();
+    async function login(e) {
+        e.preventDefault();
+        const user = {
+            email: e.target.email.value,
+            password: e.target.password.value,
+        };
+        try {
+            const response = await fetch(`${import.meta.env.VITE_LOGIN}`, {method: "POST",headers: {"Content-Type": "application/json"}, body: JSON.stringify(user)});
+            if(response.ok) {
+                const token = response.headers.get("token");
+                if(token) {
+                    sessionStorage.setItem("token", token);
+                    const data = await response.json();
+                    if(data.success) {
+                        setUser(data.data);
+                        navigate("/profile");
+                    };
+                };
+            };
+        } catch (error) {
+            //
+        }
+    };
+    return (
+        <>
+            <form onSubmit={login}>
+                <Email/>
+                <Password/>
+                <Continue/>
+            </form>
+        </>
+    );
+};
