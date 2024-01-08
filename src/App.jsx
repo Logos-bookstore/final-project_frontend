03d46c2eef5f606f4416f63a997808e19c583495
Sam <sam234@yahoo.com>
2024-01-08 10:42:47 +0100

    Adding NotFound Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useContext } from 'react';
-import { NavLink, Route, Routes } from 'react-router-dom';
-import './App.css';
-import { Context } from './context/Context';
-import Books from './pages/Books';
-import Genre from './pages/Genre';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Profile from './pages/Profile';
-import Register from './pages/Register';
+import { useContext } from "react";
+import { NavLink, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Context } from "./context/Context";
+import Books from "./pages/Books";
+import Genre from "./pages/Genre";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
 
 function App() {
   const { user, setUser } = useContext(Context);
@@ -15,7 +15,7 @@ function App() {
   const { setGenreLinkActive } = useContext(Context);
   const logout = () => {
     setUser(null);
-    sessionStorage.removeItem('token');
+    sessionStorage.removeItem("token");
   };
 
   // handler to display all books again (when Books-link clicked)
@@ -26,51 +26,52 @@ function App() {
 
   return (
     <>
-      <header key='header'>
+      <header key="header">
         <nav>
           <ul>
             <li>
-              <NavLink to='/'>Logo</NavLink>
+              <NavLink to="/">Logo</NavLink>
             </li>
           </ul>
           <ul>
             <li>
-              <NavLink to='/'>Home</NavLink>
+              <NavLink to="/">Home</NavLink>
             </li>
             <li onClick={handleBooksDisplay}>
-              <NavLink to='/books'>Books</NavLink>
+              <NavLink to="/books">Books</NavLink>
             </li>
             {!user ? (
               <>
                 <li>
-                  <NavLink to='/register'>Register</NavLink>
+                  <NavLink to="/register">Register</NavLink>
                 </li>
                 <li>
-                  <NavLink to='/login'>Login</NavLink>
+                  <NavLink to="/login">Login</NavLink>
                 </li>
               </>
             ) : (
               <>
                 <li>
-                  <NavLink to='/profile'>Profile</NavLink>
+                  <NavLink to="/profile">Profile</NavLink>
                 </li>
                 <li onClick={logout}>
-                  <NavLink to='/'>Logout</NavLink>
+                  <NavLink to="/">Logout</NavLink>
                 </li>
               </>
             )}
           </ul>
         </nav>
       </header>
-      <main key='main'>
+      <main key="main">
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/books' element={<Books />}>
-            <Route path=':genre' element={<Genre />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />}>
+            <Route path=":genre" element={<Genre />} />
           </Route>
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
     </>
