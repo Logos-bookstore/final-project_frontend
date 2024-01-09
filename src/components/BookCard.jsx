3ed8bad5e8c14b6cc127c6463193cf3a8d7ab1cf
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2024-01-09 10:30:38 +0100

    creates SingleBook-page / changes the Genre-URL

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,8 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function BookCard({ book }) {
+  const navigate = useNavigate();
+
+  const handleGoToDetailsPage = () => {
+    // FOR LATER --> add the TITLE to the URL as a 2nd dynamic PARAM ?
+    /* const bookTitle = book.title.split(' ').join('-');
+    navigate(`/singlebook/${bookTitle}`); */
+    navigate(`/singlebook/${book._id}`);
+  };
+
   return (
     <div key={book._id} className='single-book-container'>
-      <img src={book.image.thumbnail} alt="" />
-      <h2>{book.title}</h2>
+      <img src={book.image.thumbnail} alt='' onClick={handleGoToDetailsPage} />
+      <h2 onClick={handleGoToDetailsPage}>{book.title}</h2>
       <h3>
         {book.author.firstName} {book.author.lastName}
       </h3>
---
+++ src/components/CartBtn.jsx
@@ -0,0 +1,3 @@
+export default function CartBtn() {
+  return <button>add to cart</button>;
+}
