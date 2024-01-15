bef353c0dd368fd2fddff2ca38f258ffe99d91fd
Sam <sam234@yahoo.com>
2024-01-15 10:51:24 +0100

    Added DeletedAccount Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { useContext } from 'react';
-import { NavLink, Route, Routes, useLocation, useNavigate } from 'react-router-dom';
-import './App.css';
-import { Context } from './context/Context';
-import Books from './pages/Books';
-import Genre from './pages/Genre';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Profile from './pages/Profile';
-import Register from './pages/Register';
-import NotFound from './pages/NotFound';
-import SearchResult from './pages/SearchResult';
-import SingleBook from './pages/SingleBook';
-import Selection from './pages/Selection';
+import { useContext } from "react";
+import {
+  NavLink,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import "./App.css";
+import { Context } from "./context/Context";
+import Books from "./pages/Books";
+import Genre from "./pages/Genre";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+import SearchResult from "./pages/SearchResult";
+import SingleBook from "./pages/SingleBook";
+import Selection from "./pages/Selection";
+import DeletedAccount from "./pages/DeletedAccount";
 
 function App() {
   const navigate = useNavigate();
@@ -20,12 +27,12 @@ function App() {
 
   const logout = () => {
     setUser(null);
-    sessionStorage.removeItem('token');
+    sessionStorage.removeItem("token");
   };
 
   const handleSearch = async (e) => {
     e.preventDefault();
-    const plused = e.target.search.value.split(' ').join('+');
+    const plused = e.target.search.value.split(" ").join("+");
     //console.log(plused);
     navigate(`/books/request/search?q=${plused}`);
   };
@@ -38,67 +45,65 @@ function App() {
 
   return (
     <>
-      <header key='header'>
+      <header key="header">
         <nav>
           <ul>
             <li>
-              <NavLink to='/'>Logo</NavLink>
+              <NavLink to="/">Logo</NavLink>
             </li>
           </ul>
           <ul>
             <li>
               <form onSubmit={handleSearch}>
-                <input type='text' name='search' id='search' />
-                <button type='submit'>search</button>
+                <input type="text" name="search" id="search" />
+                <button type="submit">search</button>
               </form>
             </li>
           </ul>
           <ul>
             <li>
-              <NavLink to='/'>Home</NavLink>
+              <NavLink to="/">Home</NavLink>
             </li>
             <li onClick={handleBooksDisplay}>
-              <NavLink to='/books'>Books</NavLink>
+              <NavLink to="/books">Books</NavLink>
             </li>
             {!user ? (
               <>
                 <li>
-                  <NavLink to='/register'>Register</NavLink>
+                  <NavLink to="/register">Register</NavLink>
                 </li>
                 <li>
-                  <NavLink to='/login'>Login</NavLink>
+                  <NavLink to="/login">Login</NavLink>
                 </li>
               </>
             ) : (
               <>
                 <li>
-                  <NavLink to='/profile'>Profile</NavLink>
+                  <NavLink to="/profile">Profile</NavLink>
                 </li>
                 <li onClick={logout}>
-                  <NavLink to='/'>Logout</NavLink>
+                  <NavLink to="/">Logout</NavLink>
                 </li>
               </>
             )}
           </ul>
         </nav>
       </header>
-      <main key='main'>
+      <main key="main">
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/books' element={<Books />}>
-            <Route
-              path='/books/genre/:genre'
-              element={<Genre />}
-            />
-            <Route path='/books/selection' element={<Selection/>} />
-            <Route path='/books/singlebook/:id' element={<SingleBook />} />
-            <Route path='/books/request/:search' element={<SearchResult />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />}>
+            <Route path="/books/genre/:genre" element={<Genre />} />
+            <Route path="/books/selection" element={<Selection />} />
+            <Route path="/books/singlebook/:id" element={<SingleBook />} />
+            <Route path="/books/request/:search" element={<SearchResult />} />
           </Route>
-          
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='*' element={<NotFound />} />
+
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/deletedAccount" element={<DeletedAccount />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
     </>
