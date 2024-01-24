f5df7b721eb07284536e1f7a2286ce3d889467b2
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2024-01-24 14:18:23 +0100

    renders errorMSGs on the login-page

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -1,9 +1,22 @@
-export default function Email() {
+export default function Email({ loginError, setLoginError }) {
+  const handleErrorOnFocus = () => {
+    if (loginError === 'Please make sure your email is correct.')
+      setLoginError('');
+  };
+
   return (
     <>
-      <div className="email-container">
-        <label className="email-label" htmlFor="email">Email</label>
-        <input className="email-input" type="email" name="email" id="email" />
+      <div className='email-container'>
+        <label className='email-label' htmlFor='email'>
+          Email
+        </label>
+        <input
+          onFocus={handleErrorOnFocus}
+          className='email-input'
+          type='email'
+          name='email'
+          id='email'
+        />
       </div>
     </>
   );
