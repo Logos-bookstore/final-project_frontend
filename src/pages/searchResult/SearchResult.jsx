cdb8f8db2c98b8c65844a57d65c7fbc8fe70550a
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2024-01-25 16:45:58 +0100

    adds pagination (Genre, SearchResult), moves 'booksPerPage' state to Context

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -6,10 +6,19 @@ import DeleteBtnAdmin from '../../components/DeleteBtnAdmin';
 import DeleteBook from '../../components/DeleteBook';
 import UpdateBtnAdmin from '../../components/UpdateBtnAdmin';
 import UpdateBook from '../../components/UpdateBook';
+import Pagination from '../../components/Pagination';
 
 export default function SearchResult() {
-  const { user, booksToGenre, setBooksToGenre, bookToUpdate, bookToDelete } =
-    useContext(Context);
+  const {
+    user,
+    booksToGenre,
+    setBooksToGenre,
+    bookToUpdate,
+    bookToDelete,
+    currentPage,
+    setCurrentPage,
+    booksPerPage,
+  } = useContext(Context);
   const queryString = new URLSearchParams(window.location.search);
   const setQueryString = () => {
     let result;
@@ -19,6 +28,7 @@ export default function SearchResult() {
     return result;
   };
   const ourUrl = setQueryString().split(' ').join('+');
+
   useEffect(() => {
     async function bookSearch() {
       try {
@@ -29,6 +39,7 @@ export default function SearchResult() {
           const data = await response.json();
           if (data.success) {
             setBooksToGenre(data.data);
+            setCurrentPage(1);
           }
         }
       } catch (error) {
@@ -38,13 +49,18 @@ export default function SearchResult() {
     bookSearch();
   }, [ourUrl]);
 
+  // get current books
+  const indexOfLastBook = currentPage * booksPerPage;
+  const indexOfFirstBook = indexOfLastBook - booksPerPage;
   const sortedBooks = booksToGenre.sort((a, b) =>
     a.author.split(' ').at(-1).localeCompare(b.author.split(' ').at(-1))
   );
+  const currentBooks = sortedBooks.slice(indexOfFirstBook, indexOfLastBook);
+
   return (
     <>
       <div className='books-container'>
-        {sortedBooks.map((book) => (
+        {currentBooks.map((book) => (
           <div key={book._id}>
             <BookCard book={book} />
             <CartBtn book={book} />
@@ -55,6 +71,10 @@ export default function SearchResult() {
           </div>
         ))}
       </div>
+      <Pagination
+        booksPerPage={booksPerPage}
+        totalBooks={booksToGenre.length}
+      />
     </>
   );
 }
